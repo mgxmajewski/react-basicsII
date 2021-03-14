5ee72768a9fa13ddec06fc4fdebe86dcf0c3e2f8
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-14 19:41:41 +0100

    Add componentDidMount and time methods and logic to keep track of time

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,13 +3,32 @@ import React, {Component} from "react";
 class Stopwatch extends Component {
 
     state = {
-        isRunning: false
+        isRunning: false,
+        elapsedTime: 0,
+        previousTime: 0
+    }
+
+    componentDidMount() {
+        this.intervalID = setInterval(() => this.tick(), 100);
+    }
+
+    tick = () => {
+        if (this.state.isRunning) {
+            const now = Date.now()
+            this.setState( prevState => ({
+                previousTime: now,
+                elapsedTime: prevState.elapsedTime + (now - this.state.previousTime)
+            }))
+        }
     }
 
     handleStopwatch = () => {
-        this.setState({
+        this.setState(prevState => ({
             isRunning: !this.state.isRunning
-        })
+        }))
+        if (!this.state.isRunning) {
+            this.setState({previousTime: Date.now()})
+        }
     }
 
     render() {
