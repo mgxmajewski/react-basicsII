3cd6ac6184de2b5d455184e4891f2df474354d75
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-14 21:28:16 +0100

    Add PropTypes as static to Player component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,18 @@
 import React, {PureComponent} from "react";
 import Counter from "./Counter";
+import PropTypes from "prop-types";
 
 class Player extends PureComponent {
+
+    static propTypes = {
+        changeScore: PropTypes.func,
+        removePlayer: PropTypes.func,
+        name: PropTypes.string,
+        score: PropTypes.number,
+        id: PropTypes.number,
+        index: PropTypes.number,
+    }
+
     render() {
         const {removePlayer, id, score, index, name, changeScore} = this.props;
         return (
